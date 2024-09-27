def4b68d145827f382da456a3365dc1297f94457
vix <churink9@gmail.com>
2024-09-27 18:59:51 +0300

    first commit

---
+++ 1.Sorting by choice/1.Sorting by choice.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+#include <algorithm>
+
+int find_minimum(const std::vector<int>& arr, int start, int end) 
+{
+    int min_index = start;
+    for (int i = start + 1; i < end; ++i) 
+    {
+        if (arr[i] < arr[min_index]) 
+        {
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+void selection_sort(std::vector<int>& arr) 
+{
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; ++i) 
+    {
+        std::promise<int> promise;
+        std::future<int> future = promise.get_future();
+
+        std::thread([&, i, n, promise = std::move(promise)]() mutable 
+            {
+            int min_index = find_minimum(arr, i, n);
+            promise.set_value(min_index);
+            }).detach(); 
+
+        int min_index = future.get();
+
+        std::swap(arr[i], arr[min_index]);
+    }
+}
+
+int main() 
+{
+    std::vector<int> arr = { 210, 25, 12, 22, 11 };
+
+    std::cout << "Original array: ";
+    for (int num : arr) 
+    {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    selection_sort(arr);
+
+    std::cout << "Sorted array: ";
+    for (int num : arr) 
+    {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+} 
---
+++ For_each/For_each.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <numeric>
+
+template<typename Iterator, typename Function>
+void parallel_for_each(Iterator begin, Iterator end, Function func) 
+{
+    auto length = std::distance(begin, end);
+    if (length <= 1000) 
+    { 
+        std::for_each(begin, end, func);
+    }
+    else 
+    {
+        auto mid = begin + length / 2;
+
+        std::thread left_thread(parallel_for_each<Iterator, Function>, begin, mid, func);
+        std::thread right_thread(parallel_for_each<Iterator, Function>, mid, end, func);
+
+        left_thread.join();
+        right_thread.join();
+    }
+}
+
+int main() 
+{
+    std::vector<int> vec(1500);
+    std::iota(vec.begin(), vec.end(), 1); 
+
+    auto print = [](int n) 
+    {
+      std::cout << n << " ";
+    };
+
+    parallel_for_each(vec.begin(), vec.end(), print);
+
+    return 0;
+}
